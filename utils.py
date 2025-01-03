969ed7c8390271c214e0b06a6861a938ab60dce9
D-A-N-1-3-6 <158775247+D-A-N-1-3-6@users.noreply.github.com>
2025-01-03 12:34:31 +0100

    added some functions

--- utils.py
+++ utils.py
@@ -1,8 +1,27 @@
+# Image Processing
 import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
+import os
 
-# Some image processing functions
+# Model building
+import torch
+import torch.nn as nn
+from torch.utils.data import dataloader
+from torchvision import datasets
+from torchvision import transforms
+from torchvision.transforms import ToTensor
+from torchmetrics import Accuracy
+
+from tqdm.auto import tqdm
+
+torch.manual_seed(42)
+
+
+def walk_through_dir(dir_path):
+    for dirpath, dirnames, filenames in os.walk(dir_path):
+        print(f"There are {len(dirnames)} directories and {len(filenames)} images in '{dirpath}'.")
+
 
 def load_image(image_path, size, show=False):
     original_img = Image.open(image_path)
@@ -51,5 +70,83 @@ def rotate_image(image, angle, view=False):
         plt.show()
     return image_rotated
 
+
+def accuracy_fn(y_true, y_pred):
+    correct = torch.eq(y_true, y_pred).sum().item()
+    acc = (correct / len(y_pred)) * 100
+    return acc
+
+
+def train(Epochs: int,
+        dataloader: dataloader,
+        Model: nn.Module,
+        loss_fn: nn.Module,
+        optimizer: torch.optim.Optimizer,
+        accuracy_fn=accuracy_fn,
+        device=None,
+        show_loss_curve=False):
+    # For plotting
+    loss_list = []
+    train_loss, acc = 0, 0
+    
+    for epoch in tqdm(range(Epochs)):
+        print(f"Epoch: {epoch}\n -----")
+        Model.train()
+        for batch, (X, y) in enumerate(dataloader):
+            if device is not None:
+                Model = Model.to(device)
+                X, y = X.to(device), y.to(device)
+            
+            y_pred = Model(X)
+            loss  = loss_fn(y_pred, y)
+            loss_list.append(loss)
+            train_loss += loss
+            acc = accuracy_fn(y, y_pred.argmax(dim=1))
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+        print(f"Test loss: {train_loss:.5f} | Test acc: {acc:.2f}%\n")
+    
+
+def test(dataloader: dataloader,
+        Model: nn.Module,
+        loss_fn: nn.Module,
+        optimizer: torch.optim.Optimizer,
+        accuracy_fn=accuracy_fn,
+        device=None,):
+    loss, acc = 0, 0
+    Model.eval()
+    with torch.inference_mode():
+        for batch, (X, y) in enumerate(dataloader):
+            if device is not None:
+                X, y = X.to(device), y.to(device)
+            
+            y_pred = Model(X)
+            loss += loss_fn(y_pred, y)
+            acc += accuracy_fn(y_true=y, y_pred=y_pred.argmax(dim=1))
+
+            print(f"Test loss: {loss:.5f} | Test acc: {acc:.2f}%\n") 
+
+def eval(model: torch.nn.Module, 
+        data_loader: torch.utils.data.DataLoader, 
+        loss_fn: torch.nn.Module, 
+        accuracy_fn):
+    loss, acc = 0, 0
+    model.eval()
+    with torch.inference_mode():
+        for X, y in tqdm(data_loader):
+            y_pred = model(X)
+            loss += loss_fn(y_pred, y)
+            acc += accuracy_fn(y_true=y, 
+                                y_pred=y_pred.argmax(dim=1))
+        
+        loss /= len(data_loader)
+        acc /= len(data_loader)
+        
+    return {"model_name": model.__class__.__name__,
+            "model_loss": loss.item(),
+            "model_acc": acc}
 
 
